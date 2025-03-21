a2a9f02e4914ed4a4d0a85d12c86c22ebf2e1cee
HayatoYunoki <yunoki-hayato@g.ecc.u-tokyo.ac.jp>
2025-03-21 10:18:35 +0900

    公開テスト

--- make.bat
+++ make.bat
@@ -10,6 +10,16 @@ if "%SPHINXBUILD%" == "" (
 set SOURCEDIR=source
 set BUILDDIR=build
 
+if "%1" == "" goto help
+
+if "%1" == "preview" (
+    echo.Running sphinx-autobuild. View live edits at:
+    echo.  http://0.0.0.0:8000
+    sphinx-autobuild -b html %SOURCEDIR% %BUILDDIR%/html
+    if errorlevel 1 exit /b 1
+    goto end
+)
+
 %SPHINXBUILD% >NUL 2>NUL
 if errorlevel 9009 (
 	echo.
@@ -19,11 +29,13 @@ if errorlevel 9009 (
 	echo.may add the Sphinx directory to PATH.
 	echo.
 	echo.If you don't have Sphinx installed, grab it from
-	echo.https://www.sphinx-doc.org/
+	echo.http://sphinx-doc.org/
 	exit /b 1
 )
 
-if "%1" == "" goto help
++:html
++%SPHINXBUILD% -b html %SOURCEDIR% "docs" %SPHINXOPTS% %O%
++goto end
 
 %SPHINXBUILD% -M %1 %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
 goto end
